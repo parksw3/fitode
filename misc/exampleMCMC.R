21eb66964eeda4abe88437d415de6af16c9bcb9d
unknown <parksw3@mcmaster.ca>
2019-07-08 02:33:03 -0400

    tweak to examples

--- misc/exampleMCMC.R
+++ misc/exampleMCMC.R
@@ -20,43 +20,22 @@ model <- new("model.ode",
 
 ## weird prior just for testing
 prior <- list(
-    beta~dgamma(shape=100, rate=50)
+    beta~dgamma(shape=2, rate=1)
 )
 
 parms <-c(beta=1,gamma=0.5, N=1000, i0=1e-2, size1=10, size2=10)
 
 df <- simulate(model, times=1:20, parms=parms, seed=101)[,c("times", "susceptible", "infected")]
 
-system.time(ff <- fitode(
-    model=model,
-    data=df,
-    start=parms
-))
-
-system.time(ff2 <- fitode(
+system.time(ff <- fitodeMCMC(
     model=model,
     data=df,
     start=parms,
-    prior=prior
-))
-
-system.time(ff3 <- fitode(
-    model=model,
-    data=df,
-    start=parms,
+    vcov = 1*diag(length(parms)),
     prior=prior,
-    prior.density = FALSE
+    chains=2, thin=10,
+    prior.only=TRUE
 ))
 
-plot(ff, level=0.95)
-plot(ff, level=0.95, method="wmvrnorm")
+plot(as.mcmc.list(ff[[1]])[,1])
 
-plot(ff2, level=0.95)
-plot(ff2, level=0.95, method="wmvrnorm")
-
-plot(ff3, level=0.95)
-plot(ff3, level=0.95, method="wmvrnorm")
-
-confint(ff2, parm="beta")
-confint(ff2, parm="beta", method="wmvrnorm")
-confint(ff2, parm="beta", method="profile")
